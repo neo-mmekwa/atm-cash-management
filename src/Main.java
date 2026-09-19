6eb414ffd6e2be48e1d98c44abb0e5d3ddb1535b
Neo Mmekwa <neommekwa243@gmail.com>
2026-09-20 00:37:12 +0200

    Initial project setup

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * @author neo mmekwa 
+ */
+
+
+public class Main 
+{
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("WELCOME TO THE ATM CASH MANAGEMENT SYSTEM!");
+	}
+}
